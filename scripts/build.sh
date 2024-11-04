9e22966f8ca82548f1a77c66c8764f9cdc5e1026
Matteo Gastaldello <matteo.gastaldellomiotto@gmail.com>
2024-11-04 15:02:16 +0100

    chore: wrong build references

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,9 +2,9 @@
 
 # KO_DOCKER_REPO=kind.local ko build --base-import-paths .  --preserve-import-paths
 
-# KO_DOCKER_REPO=kind.local KIND_CLUSTER_NAME=krateo-quickstart ko build --base-import-paths .
-KO_DOCKER_REPO=matteogastaldello ko build -t 0.1.4 --base-import-paths .
-# printf '\n\nList of current docker images loaded in KinD:\n'
+KO_DOCKER_REPO=kind.local KIND_CLUSTER_NAME=krateo-quickstart ko build --base-import-paths .
+# KO_DOCKER_REPO=matteogastaldello ko build -t 0.1.4 --base-import-paths .
+printf '\n\nList of current docker images loaded in KinD:\n'
 
-# kubectl get nodes krateo-quickstart-control-plane -o json \
-#     | jq -r '.status.images[] | " - " + .names[-1]'
+kubectl get nodes krateo-quickstart-control-plane -o json \
+    | jq -r '.status.images[] | " - " + .names[-1]'
